2db3861dfb63bdabaf4926034d728aa7db1db63f
itspitso <147466450+itspitso@users.noreply.github.com>
2024-12-10 15:20:56 +0200

    Add extra script

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,5 @@
+income = float(input('Enter your monthly income:'))
+expenses = float(input('Enter your total monthly expenses:'))
+savings = income - expenses
+rate = 0.05
+projected_savings = savings * 12 + (savings * 12 * 0.05)
