9ac2530f633e0da24e8282a876422f1fea43a843
itspitso <147466450+itspitso@users.noreply.github.com>
2024-12-19 06:30:49 +0200

    Fix error in pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,9 @@
 size = input('Enter the size of the pattern:')
 size_int = int(size)
 symbol = '*'
-for i in range(1, size_int+1):
-    print(f'{symbol*size_int}')
+i = 0
+while i < size_int: 
+    for j in range(1, size_int+1):
+        print(symbol, end="")
+    print('\n')
+    i += 1
