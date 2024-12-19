30b6f254bc0b65f49f38efbfadcbffa670479ac5
itspitso <147466450+itspitso@users.noreply.github.com>
2024-12-19 08:37:20 +0200

    Changed the print statement to include the asterisk as a direct string instead of as a variable

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,8 +1,7 @@
 size = int(input('Enter the size of the pattern:'))
-symbol = '*'
 i = 0
 while i < size: 
     for j in range(1, size+1):
-        print(symbol, end="")
+        print('*', end="")
     print('\n')
     i += 1
