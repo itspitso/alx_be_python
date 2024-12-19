a48bd9c4a51aa2a285d15abd9ee1834254d3b9f5
itspitso <147466450+itspitso@users.noreply.github.com>
2024-12-19 09:24:37 +0200

    Added the words reminder and note to the messages in daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,11 +4,11 @@ time_bound = input("Is it time-bound? (yes/no):")
 
 match priority:
     case 'high':
-        message = f'{task} is a high priority task'
+        message = f'Reminder: {task} is a high priority task'
     case 'medium':
         message = f'{task} is a medium priority task'
     case 'low':
-        message = f'{task} is a low priority task'
+        message = f'Note: {task} is a low priority task'
 
 if time_bound == 'yes':
     message += ' that requires immediate attention today!'
