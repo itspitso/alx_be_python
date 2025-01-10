bc441aa310467906b712c54e245ef80b0fac2b40
itspitso <147466450+itspitso@users.noreply.github.com>
2025-01-10 06:27:03 +0200

    Changed logic from match case to if/else

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,13 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case 'add':
-            return float(num1 + num2)
-        case 'subtract':
-            return float(num1 - num2)
-        case 'multiply':
-            return float(num1 * num2)
-        case 'divide':
-            try:
-                result = float(num1 / num2)
-                return result
-            except ZeroDivisionError:
-                return "Cannot divide by zero"
+    if operation == 'add': 
+        return float(num1 + num2)
+    elif operation == 'subtract':
+        return float(num1 - num2)
+    elif operation == 'multiply':
+        return float(num1 * num2)
+    elif operation == 'divide':
+        try:
+            result = float(num1 / num2)
+            return result
+        except ZeroDivisionError:
+            return "Cannot divide by zero"
