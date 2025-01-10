2752a983c49a22470d37f5a371a4149ceaa707c7
itspitso <147466450+itspitso@users.noreply.github.com>
2025-01-10 06:30:55 +0200

    Replaced try/except block for checking zero divison with equal operator that checks whether the second number is equal to zero

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,8 +6,8 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return float(num1 * num2)
     elif operation == 'divide':
-        try:
-            result = float(num1 / num2)
-            return result
-        except ZeroDivisionError:
+        if num2 == 0:
             return "Cannot divide by zero"
+        else:
+            return float(num1 / num2)
+
