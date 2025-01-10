0b4906bb8619dd3946ac712c0f1ef7caa6f25ca7
itspitso <147466450+itspitso@users.noreply.github.com>
2025-01-10 06:20:59 +0200

    Changed the function to return floats instead of integers

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
 def perform_operation(num1, num2, operator):
     match operator:
         case 'add':
-            return num1 + num2
+            return float(num1 + num2)
         case 'subtract':
-            return num1 - num2
+            return float(num1 - num2)
         case 'multiply':
-            return num1 * num2
+            return float(num1 * num2)
         case 'divide':
             try:
-                result = num1 / num2
+                result = float(num1 / num2)
                 return result
             except ZeroDivisionError:
                 return "Cannot divide by zero"
