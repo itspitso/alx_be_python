0cd441878d8902a98c888ca52d9a1793318e4073
itspitso <147466450+itspitso@users.noreply.github.com>
2025-01-12 21:30:09 +0200

    Fixed function name from display_date to display_datetime which is the correct function name

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,6 +1,6 @@
 from datetime import datetime, date, timedelta
 
-def display_current_date():
+def display_current_datetime():
     current_date = datetime.now()
     print(f'Current date and time: {current_date}')
 
@@ -10,7 +10,7 @@ def calculate_future_date(number_of_days):
     print(f'Future date: {future_date}')
 
 def main():
-    display_current_date()
+    display_current_datetime()
     number_of_days = int(input('Enter the number of days to add to the current date: '))
     calculate_future_date(number_of_days)
 
