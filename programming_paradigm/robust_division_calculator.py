cda7f513c6cf08bad3ad99f1604b3174f803d3f4
itspitso <147466450+itspitso@users.noreply.github.com>
2025-01-19 12:58:19 +0200

    Fixed return statement in else block

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,12 +1,12 @@
 def safe_divide(numerator, denominator):
     try:
-        float(numerator) / float(denominator)
+        result = float(numerator) / float(denominator)
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
     else:
-        return float(numerator) / float(denominator)
+        return f"The result of the division is {result}."
         
     
     
