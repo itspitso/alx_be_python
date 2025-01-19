85963a6018a4c1a7e5eb881210c3a47b696e8a47
itspitso <147466450+itspitso@users.noreply.github.com>
2025-01-19 12:08:57 +0200

    Added default attribute value

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,8 @@
 class BankAccount:
-    def __init__(self, account_balance):
-        self.account_balance = account_balance
+    account_balance = 0
+    def __init__(self, account_balance=None):
+        if account_balance is not None:
+            self.account_balance = account_balance
 
     def deposit(self, amount):
         self.account_balance += amount
