6697f3597e739b385a621006600ad5ae45b39df9
itspitso <147466450+itspitso@users.noreply.github.com>
2025-01-19 12:52:01 +0200

    Fixed string to print when catching zero division error

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,7 +2,7 @@ def safe_divide(numerator, denominator):
     try:
         float(numerator) / float(denominator)
     except ZeroDivisionError:
-        return "Error: Cannot divide by zero"
+        return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please use numeric values only."
     else:
