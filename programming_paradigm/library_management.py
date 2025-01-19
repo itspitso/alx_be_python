ee82ff106105110a5d30c03fce2e093d48d9f2d7
itspitso <147466450+itspitso@users.noreply.github.com>
2025-01-19 23:41:42 +0200

    Fixed variable name for list of available books

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -13,23 +13,23 @@ class Book:
 class Library():
 
     def __init__(self):
-        self.available_books = []
+        self._books = []
 
     def add_book(self, book):
         if isinstance(book, Book):  # Check if the parameter is a Book object
-            self.available_books.append([book.title, book.author, book.is_checked_out])
+            self._books.append([book.title, book.author, book.is_checked_out])
 
     def list_available_books(self):
-        for book in self.available_books:
+        for book in self._books:
             if book[2] != True:
                 print(f"{book[0]} by {book[1]}")
 
     def check_out_book(self, title):
-        for book in self.available_books:
+        for book in self._books:
             if title == book[0] and book[2] == False:
                 book[2] = True
 
     def return_book(self, title):
-        for book in self.available_books:
+        for book in self._books:
             if book[2] == True:
                 book[2] = False
